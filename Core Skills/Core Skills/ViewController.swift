58da2c7afad050dff8b783501d3fbbd263a2182a
Andrew Clark (RIT Student) <alc7546@twctechimac01.main.ad.rit.edu>
2019-01-22 14:43:25 -0500

    Finished Core Skills from library

--- Core Skills/Core Skills/ViewController.swift
+++ Core Skills/Core Skills/ViewController.swift
@@ -8,18 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+// UITextFieldDelegate is used for handling text field events, I think it's a subclass of UIViewC
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    // Command + K for showing on-screen keyboard
+    // Connected variables
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        label.text = "Hello from code!"
+        // Prompts keyboard for textfield
+        textField.becomeFirstResponder()
+        // The object its going to handle the event
+        textField.delegate = self
+        
+    }
+    
+    
+    // Connected action from the button - checks if it was tapped, if so change text and resign
+    @IBAction func buttonWasTapped(_ sender: Any) {
+        
+        label.text = "Hello \(textField.text!)"
+        // Removes keyboard as first responder from textfield
+        textField.resignFirstResponder()
+        
+    }
+    
+    // Touched elsewhere - resign
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        textField.resignFirstResponder()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // Return button clicked - resign all, don't line break
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.endEditing(true)
+        return false
     }
-
 
 }
 
