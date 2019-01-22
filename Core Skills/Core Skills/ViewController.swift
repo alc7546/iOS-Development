869e41fab28f525c8ac3d3cbab77174c3bb0465c
Andrew Clark (RIT Student) <alc7546@twctechimac01.main.ad.rit.edu>
2019-01-22 13:40:19 -0500

    Core Skills

---
+++ Core Skills/Core Skills/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Core Skills
+//
+//  Created by Andrew Clark (RIT Student) on 1/22/19.
+//  Copyright © 2019 Andrew Clark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
