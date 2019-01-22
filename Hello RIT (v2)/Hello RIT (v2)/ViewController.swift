842fef20156b2fb51c3e2af54261f9513a3e89c6
Andrew Clark (RIT Student) <alc7546@twctechimac01.main.ad.rit.edu>
2019-01-22 12:28:01 -0500

    Completed Hello RIT-1 from Library

--- Hello RIT (v2)/Hello RIT (v2)/ViewController.swift
+++ Hello RIT (v2)/Hello RIT (v2)/ViewController.swift
@@ -1,6 +1,7 @@
 //
 //  ViewController.swift
 //  Hello RIT (v2)
+// Running 9.2 xCode
 //
 //  Created by Andrew Clark (RIT Student) on 1/22/19.
 //  Copyright © 2019 Andrew Clark. All rights reserved.
@@ -10,15 +11,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var topLabel:UILabel = UILabel()
+    var bottomLabel:UILabel = UILabel()
+    var footerLabel:UILabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        topLabel.text = "Hello"
+        topLabel.font = UIFont.systemFont(ofSize: 36)
+        topLabel.sizeToFit()
+        view.addSubview(topLabel)
+        topLabel.center = CGPoint(x:100, y:40)
+        
+        bottomLabel.text = "RIT"
+        bottomLabel.font = UIFont.boldSystemFont(ofSize: 48)
+        bottomLabel.sizeToFit()
+        view.addSubview(bottomLabel)
+        bottomLabel.center = CGPoint(x: 200, y:90)
+        
+        view.backgroundColor = UIColor.orange
+        
+        footerLabel.text = "Andrew Clark"
+        footerLabel.font = UIFont.italicSystemFont(ofSize: 24)
+        footerLabel.sizeToFit()
+        footerLabel.textColor = UIColor.gray
+        view.addSubview(footerLabel)
+        // view.frame.width/height
+        footerLabel.center = CGPoint(x:view.frame.width/2, y:view.frame.height - 50)
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
