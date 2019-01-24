6853bb9e1988bd7c6717bbc8e0042a4ac9bee2fb
Andrew Clark (RIT Student) <alc7546@twcviaimac09.main.ad.rit.edu>
2019-01-24 14:55:19 -0500

    Motivator ICE Start - from Library

---
+++ Motivator-ICE/Motivator-ICE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Motivator-ICE
+//
+//  Created by Andrew Clark (RIT Student) on 1/24/19.
+//  Copyright © 2019 Andrew Clark (RIT Student). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
